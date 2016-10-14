e7fa54728949ec825acf2e4c5027ee64dae8df95
Luv Letter <luvletter@Luvs-Mac-mini.local>
2016-10-15 01:10:23 +0800

    deploy center

---
+++ hc-go/light.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/LuvLetter/hc"
+	"github.com/LuvLetter/hc/accessory"
+	"log"
+)
+
+func turnLightOn() {
+	log.Println("Turn Light On")
+}
+
+func turnLightOff() {
+	log.Println("Turn Light Off")
+}
+
+func main() {
+	info := accessory.Info{
+		Name:         "Personal Light Bulb",
+		Manufacturer: "Matthias",
+	}
+
+	acc := accessory.NewLightbulb(info)
+
+	acc.Lightbulb.On.OnValueRemoteUpdate(func(on bool) {
+		if on == true {
+			turnLightOn()
+		} else {
+			turnLightOff()
+		}
+	})
+
+	t, err := hc.NewIPTransport(hc.Config{Pin: "32191123"}, acc.Accessory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hc.OnTermination(func() {
+		t.Stop()
+	})
+
+	t.Start()
+}
---
+++ hc-go/tempture.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/LuvLetter/hc"
+	"github.com/LuvLetter/hc/accessory"
+	"log"
+)
+
+func GetTempture(){
+
+}
+
+func main() {
+	info := accessory.Info{
+		Name:         "Generic Tempture Sensor",
+		Manufacturer: "HDU LUG",
+	}
+  for i := 0; true; i++ {
+    TemptureSensor := accessory.NewTemperatureSensor(info, GetTempture, -35, 100, 0.5)
+  }
+
+	t, err := hc.NewIPTransport(hc.Config{Pin: "32191123"}, TemptureSensor.Accessory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+}
